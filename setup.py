0a750ce26324bbc2a2f34f5c989f2808e0bacca4
Mathias Kraus <mathias.sebastian.kraus@gmail.com>
2023-06-30 23:58:56 +0200

    update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='igann',
-    version='0.1.0',
+    version='0.1.1',
     author='Mathias Kraus',
     author_email='mathias.sebastian.kraus@gmail.com',
     description='Implementation of Interpretable Generalized Additive Neural Networks',
