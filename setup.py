00d02c8e31f0aa734469781a8b7b5c6dadfffaca
Mathias Kraus <mathias.sebastian.kraus@gmail.com>
2023-06-30 22:08:43 +0200

    * add setup file

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='igann',
+    version='0.1.0',
+    author='Mathias Kraus',
+    author_email='mathias.kraus@fau.de',
+    description='Implementation of Interpretable Generalized Additive Neural Networks',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/MathiasKraus/igann',
+    license='MIT',
+    packages=['igann'],
+    zip_safe=False,
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'torch>=1.9.0',
+        'matplotlib',
+        'seaborn',
+        'abess==0.4.5'
+    ]
+)
+
