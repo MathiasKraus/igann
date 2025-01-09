6b3e7f112f096fa32e75d4fafc3110ed817d0234
HB_Dynamite <lasse.bohlen@fau.de>
2025-01-09 13:10:39 +0100

    update _init_ to handle igann extensions

--- igann/__init__.py
+++ igann/__init__.py
@@ -1,4 +1,6 @@
 from .igann import IGANN
+from .igann import ELM_Regressor
+from .igann_interactive import IGANN_interactive
 
 # from .igann_bagged import IGANN_Bagged
 from .igannSklearn import IGANNClassifier, IGANNRegressor
